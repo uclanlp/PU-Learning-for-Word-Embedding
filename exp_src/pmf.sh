3b00850d079ed9a036638e7bb012d1ceff107e24
Chao  Jiang <cj7an@nlp.cs.virginia.edu>
2017-02-23 21:39:28 -0500

    1. write pmf.sh; 2.add cooccurForAscii.c, the difference is that there is a parameter ascii=1 makes cooccurence matrix is outputed in ascii format; 3.edit Makefile to compile it

--- exp_src/pmf.sh
+++ exp_src/pmf.sh
@@ -3,8 +3,8 @@
 # Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
 
-cd ../GloVe-1.2/make; cd ../exp_src
-cd ../libpmf-1.6/make; cd ../exp_src
+cd ../GloVe-1.2/; make; cd ../exp_src
+cd ../libpmf-1.6/; make; cd ../exp_src
 
 if [ ! -e ../data/text8 ]; then
   if hash wget 2>/dev/null; then
@@ -15,6 +15,7 @@ if [ ! -e ../data/text8 ]; then
   unzip text8.zip
   rm text8.zip
 fi
+mkdir -p ../data
 mv text8 ../data/text8
 
 CORPUS=../data/text8
@@ -22,7 +23,9 @@ VOCAB_FILE=vocab.txt
 COOCCURRENCE_FILE=cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
 BUILDDIR=../GloVe-1.2/build
-LIBPMF=../libpmf-1.6/
+LIBPMF=../libpmf-1.6
+CONVERTER=$LIBPMF/converter
+PMF=$LIBPMF/pmf-train
 
 
 SAVE_FILE=vectors
@@ -36,42 +39,44 @@ BINARY=2
 NUM_THREADS=8
 X_MAX=10
 
-
-
-
 #### Train libpmf
+
+
 
 $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
 if [[ $? -eq 0 ]]
   then
-  $BUILDDIR/cooccur -ascii -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
+  $BUILDDIR/cooccurForAscii -ascii=1  -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
   if [[ $? -eq 0 ]]
-  then
-
-    #### NEED to write script to prepare data for pmf
-    $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
-    if [[ $? -eq 0 ]]
     then
-       $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
-       if [[ $? -eq 0 ]]
-       then
-           if [ "$1" = 'matlab' ]; then
-               matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2 
-           elif [ "$1" = 'octave' ]; then
-               octave < ./eval/octave/read_and_evaluate_octave.m 1>&2 
-           else
-               python2.7 ../eval/python/evaluate.py
-           fi
-       fi
+    lineOfVocab=$(cat <vocab.txt |wc -l)
+    numberOfNonZero=$(cat <cooccurrence.bin |wc -l)
+    cp cooccurrence.bin ../libpmf-1.6/toy-example/test.ratings
+    cp cooccurrence.bin ../libpmf-1.6/toy-example/training.ratings    
+cat>../libpmf-1.6/toy-example/meta<<EOF
+$lineOfVocab $lineOfVocab
+$numberOfNonZero training.ratings
+$numberOfNonZero test.ratings
+EOF
+        	
+    if [[ $? -eq 0 ]]
+      then
+      cd ../libpmf-1.6/
+      ./converter toy-example
+      ./omp-pmf-train -s 11 -n 12 -f 1 -k 20 toy-example
+      if [[ $? -eq 0 ]]
+        then
+        cd ../exp_src
+        cut -f 1 -d ' ' vocab.txt > word
+        paste -d ' ' word  ../libpmf-1.6/toy-example.model >pmf_embedding
+        if [[ $? -eq 0 ]]
+          then 
+          python2.7 ../eval/python/evaluate.py '--vocab_file'=vocab.txt '--vectors_file'=pmf_embedding
+        fi
+      fi
     fi
   fi
 fi
-
-
-##here is the script to create embedding based on pmf output
-
-cut -d  ' ' -f 1 vocab.txt  > words
-paste -d ' ' words ../libpmf-1.6/example.model > pmfembedding.txt
 
 
 
